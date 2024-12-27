e81739ef28af435681608f46ea63b175f302928f
bevan <bevanwsjones@hotmail.com>
2024-12-27 15:08:31 +0200

    - successfully started after transplant.

--- fyn-api/settings.py
+++ fyn-api/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+sys.path.append(str(BASE_DIR / 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -39,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'accounts',
-    'simulation_manager',
+    'apps.accounts',
+    'apps.simulation_manager',
 ]
 
 

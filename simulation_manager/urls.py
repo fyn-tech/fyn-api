7e4246da6b5c138d1a799f213c4457589e5a4a78
bevan <bevanwsjones@hotmail.com>
2024-12-27 14:38:37 +0200

    - renaming of root project.

--- simulation_manager/urls.py
+++ simulation_manager/urls.py
@@ -2,13 +2,13 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    # front end api
+    # General fyn-api
     path('simulation_submission/', views.handle_simulation_submission_form),
 
     # other app apis
     path('', include('accounts.urls')),
 
-    # backend api
+    # fyn-api admin api
     path('simulation_manager/', views.simulation_manager, name='simulation_manager'),
     path('simulation_manager/get_all_simulations/', views.get_all_simulations, name='get_all_simulations'),
     path('simulation_manager/get_user_simulations/<uuid:id>', views.get_user_simulations, name='get_user_simulations'),

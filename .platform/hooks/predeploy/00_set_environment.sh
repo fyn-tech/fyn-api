2eeecfbc0dd17064ad1ce9e0bad611bcd7750e8f
bevanwsjones <bevanwsjones@gmail.com>
2025-01-11 12:34:11 +0100

    - split environment file and exec start.

--- .platform/hooks/predeploy/00_set_environment.sh
+++ .platform/hooks/predeploy/00_set_environment.sh
@@ -9,8 +9,8 @@ PartOf=eb-app.target
 [Service]
 User=webapp
 Type=simple
-# Key change: source the environment file before starting gunicorn
-ExecStart=/bin/sh -c "source /opt/elasticbeanstalk/deployment/env && gunicorn --bind 127.0.0.1:8000 --workers=1 --threads=15 fyn-api.wsgi:application"
+EnvironmentFile=/opt/elasticbeanstalk/deployment/env
+ExecStart=/var/app/venv/staging-LQM1lest/bin/gunicorn --bind 127.0.0.1:8000 --workers=1 --threads=15 fyn-api.wsgi:application
 ExecStartPost=/bin/sh -c "systemctl show -p MainPID web.service | cut -d= -f2 > /var/pids/web.pid"
 ExecStopPost=/bin/sh -c "rm -f /var/pids/web.pid"
 Restart=always
@@ -23,4 +23,4 @@ EOL
 
 # Reload systemd and restart the web service
 systemctl daemon-reload
-systemctl restart web.service
+EOL

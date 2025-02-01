5ceea89d8508350f36dad6623f09cc13b300ace4
bevanwsjones <bevanwsjones@gmail.com>
2025-02-01 11:36:07 +0100

    created and moved settings into a package folder

--- settings/settings.py
+++ settings/settings.py
@@ -26,7 +26,7 @@ print(f"Env Vars: {env_vars}")
 print("Current ENVIRONMENT value:", os.getenv('ENVIRONMENT'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 sys.path.append(str(BASE_DIR / 'apps'))
 
 # Quick-start development settings - unsuitable for production

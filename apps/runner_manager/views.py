6287a5eb04ae87e1edd163285baed956a3392253
bevan <bevanwsjones@hotmail.com>
2025-02-15 09:09:57 +0200

    - added boiler plate for runner manager

--- apps/runner_manager/views.py
+++ apps/runner_manager/views.py
@@ -22,3 +22,60 @@ def runners(request):
         return JsonResponse({'message': 'File received successfully'}, status=200)
     
     return JsonResponse({'error': 'Invalid request'}, status=400)
+
+# -------------------------------------------------------------------------------------------------
+# Front End API
+# -------------------------------------------------------------------------------------------------
+
+@login_required
+def add_new_runner(request):
+    raise NotImplemented("WIP")
+
+
+@login_required
+def get_hardware(request):
+    raise NotImplemented("WIP")
+
+
+@login_required
+def request_new_job(request):
+    raise NotImplemented("WIP")
+
+
+@login_required
+def get_jobs(request):
+    raise NotImplemented("WIP")
+
+
+@login_required
+def get_status(request):
+    raise NotImplemented("WIP")
+
+
+# -------------------------------------------------------------------------------------------------
+# Runner API
+# -------------------------------------------------------------------------------------------------
+
+
+def register(request):
+    raise NotImplemented("WIP")
+
+
+def hardware_update(request):
+    raise NotImplemented("WIP")
+
+
+def report_status(request):
+    raise NotImplemented("WIP")
+
+
+# -------------------------------------------------------------------------------------------------
+# Genearal (multi-use) API
+# -------------------------------------------------------------------------------------------------
+
+def start_job(request):
+    raise NotImplemented("WIP")
+
+
+def terminate_job(request):
+    raise NotImplemented("WIP")

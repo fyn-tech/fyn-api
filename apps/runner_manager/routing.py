a2c51dda560b4ee53d7989d5380758b15dd7f109
bevanwsjones <bevanwsjones@gmail.com>
2025-04-05 23:15:33 +0200

    - updated routing

--- apps/runner_manager/routing.py
+++ apps/runner_manager/routing.py
@@ -1,7 +1,7 @@
 from django.urls import re_path
-from . import consumers
+from runner_manager.consumers import RunnerConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/runner_mannger/(?P<runner_id>[^/]+)/$',
-            consumers.RunnerConsumer.as_asgi()),
+    re_path(r'ws/runner_manager/(?P<runner_id>[^/]+)/$',
+            RunnerConsumer.as_asgi()),
 ]

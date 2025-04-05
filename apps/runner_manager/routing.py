2d7d496443adc2a8b2765c9e3bfa34065796b8cb
bevanwsjones <bevanwsjones@gmail.com>
2025-04-05 23:28:03 +0200

    - removed trailing slash.

--- apps/runner_manager/routing.py
+++ apps/runner_manager/routing.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from runner_manager.consumers import RunnerConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/runner_manager/(?P<runner_id>[^/]+)/$',
+    re_path(r'ws/runner_manager/(?P<runner_id>[^/]+)$',
             RunnerConsumer.as_asgi()),
 ]

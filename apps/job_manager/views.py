ac464e6cf4710fed5954cb07b0015d28874dacf7
bevanwsjones <bevanwsjones@gmail.com>
2025-06-07 11:49:56 +0200

    - cleaned imports

--- apps/job_manager/views.py
+++ apps/job_manager/views.py
@@ -1,17 +1,9 @@
-from django.http import JsonResponse, HttpResponse
-from django.contrib.auth.decorators import login_required
-from django.template import loader
-from .models import JobInfo, JobResource
-from accounts.models import User
-from django.forms import model_to_dict
-import yaml
-
-from rest_framework import viewsets, status
-from rest_framework.decorators import action
+from rest_framework import viewsets
 from rest_framework.response import Response
 from runner_manager.authentication import RunnerTokenAuthentication
 from runner_manager.permissions import IsAuthenticatedRunner
 
+from .models import JobInfo, JobResource
 from .serializers import (
     JobInfoSerializer, 
     JobInfoRunnerSerializer,
